5f9641cfb7a5723418feb00d0995dcc9dd1bd380
saidqony <saidqony@gmail.com>
2026-04-27 20:48:28 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,7 @@
-import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://berhenti-merokok.id',
   output: 'static',
-  integrations: [
-    tailwind({ applyBaseStyles: false }),
-    sitemap(),
-  ],
 });
